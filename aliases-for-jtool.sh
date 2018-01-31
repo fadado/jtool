16c2bd3ef3ec70e2ede1d64ec10d8f76ad201072
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2018-01-31 13:00:12 +0100

    Added jtool-ps

--- aliases-for-jtool.sh
+++ aliases-for-jtool.sh
@@ -2,8 +2,9 @@
 
 # Usage: source aliases.bash
 
-alias jpg=${PWD}/jtool-pager
-alias jjc=${PWD}/jtool-journal
 alias jhw=${PWD}/jtool-lshw
+alias jjc=${PWD}/jtool-journal
+alias jpg=${PWD}/jtool-pager
+alias jps=${PWD}/jtool-ps
 
 # vim:syntax=sh:et:sw=4:ts=4:ai

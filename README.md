787d3ac0d4031e1a5b27a9545ad81b549b5ed94b
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2018-02-01 13:24:43 +0100

    Started jtool script

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# jtool(s)
+# jtool &mdash; JSON tools for a modern shell
 
 JSON tools around `jq` and other utilities.
---
+++ aliases-jtool.sh
@@ -0,0 +1,11 @@
+# Short aliases for all tools
+
+# Usage: source aliases.bash
+
+alias jt="${PWD}/jtool"
+alias jhw="${PWD}/jtool lshw"
+alias jjc="${PWD}/jtool journal"
+alias jpg="${PWD}/jtool pager"
+alias jps="${PWD}/jtool ps"
+
+# vim:syntax=sh:et:sw=4:ts=4:ai

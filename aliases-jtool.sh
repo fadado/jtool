a0d1e1d11ef797664b2e0d9ed01cae8835834b4a
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2018-02-02 09:43:29 +0100

    Usa a one bigger pipe in ps

--- aliases-jtool.sh
+++ aliases-jtool.sh
@@ -5,7 +5,7 @@
 alias jt="${PWD}/jtool"
 alias jhw="${PWD}/jtool lshw"
 alias jjc="${PWD}/jtool journal"
-alias jpg="${PWD}/jtool pager"
+alias jpg="${PWD}/jtool-pager"
 alias jps="${PWD}/jtool ps"
 
 # vim:syntax=sh:et:sw=4:ts=4:ai
